4fa98b2238afff56709fe6f5cc27c4bb44f09d49
Nguyễn Thanh Tịnh <142908230+MrToInh@users.noreply.github.com>
2024-03-16 16:29:01 +0700

    first

---
+++ QuanLyLinq/DAO/AccountProvider.cs
@@ -0,0 +1,107 @@
+﻿using QuanLyLinq;
+using QuanLyQuanCaPhe.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyQuanCaPhe.DAO
+{
+    public class AccountProvider
+    {
+        private static AccountProvider instance;
+
+        public static AccountProvider Instance
+        {
+            get { if (instance == null) instance = new AccountProvider(); return instance; }
+            private set { instance = value; }
+        }
+
+
+        public bool Login(string username, string password)
+        {
+            using (QuanLyQuanCaPhe1DataContext ql = new QuanLyQuanCaPhe1DataContext())
+            {
+                var tpQuery = ql.USP_GetLogin(username, password);
+                DataTable result = LINQResultToDataTable(tpQuery);
+                return result.Rows.Count > 0;
+
+
+            }
+
+        }
+        public DataTable LINQResultToDataTable<T>(IEnumerable<T> Linqlist)
+        {
+            DataTable dt = new DataTable();
+            PropertyInfo[] columns = null;
+
+            if (Linqlist == null) return dt;
+
+            foreach (T Record in Linqlist)
+            {
+
+                if (columns == null)
+                {
+                    columns = ((Type)Record.GetType()).GetProperties();
+                    foreach (PropertyInfo GetProperty in columns)
+                    {
+                        Type colType = GetProperty.PropertyType;
+
+                        if ((colType.IsGenericType) && (colType.GetGenericTypeDefinition()
+                        == typeof(Nullable<>)))
+                        {
+                            colType = colType.GetGenericArguments()[0];
+                        }
+
+                        dt.Columns.Add(new DataColumn(GetProperty.Name, colType));
+                    }
+                }
+
+                DataRow dr = dt.NewRow();
+
+                foreach (PropertyInfo pinfo in columns)
+                {
+                    dr[pinfo.Name] = pinfo.GetValue(Record, null) == null ? DBNull.Value : pinfo.GetValue
+                    (Record, null);
+                }
+
+                dt.Rows.Add(dr);
+            }
+            return dt;
+            }
+        public AccountData GetAccount(string username)
+        {
+            using (QuanLyQuanCaPhe1DataContext ql = new QuanLyQuanCaPhe1DataContext()) {
+               
+                var query = (from Account in ql.Accounts
+                             where Account.UserName == username
+                             select Account);
+                DataTable dataTable = LINQResultToDataTable(query) ;
+                // DataProvider.Instance.ExecuteQuery("select * from account where Username= '" + username + "'");
+                foreach (DataRow item in dataTable.Rows)
+                {
+                  return new AccountData(item);
+
+                 }
+
+                return null;
+                
+            }
+        }
+        public bool UpdateInfo(string username, string displayname, string password, string newpassword)
+        {
+            using (QuanLyQuanCaPhe1DataContext ql = new QuanLyQuanCaPhe1DataContext())
+            {
+                ql.UpdateAccount(username, displayname, password, newpassword);
+                ql.SubmitChanges();
+            }
+            return true;
+            //DataTable data = DataProvider.Instance.ExecuteQuery("exec UpdateAccount @username , @displayname , @password , @newPassword", new object[] { username, displayname, password, newpassword });
+            //return data.Rows.Count > 0;
+        }
+    }
+}
